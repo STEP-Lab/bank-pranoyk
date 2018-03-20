46f429762393b7915f622eb42dc5313d7b66f5c0
Pranoy Kumar Kundu <pranoy1998k@gmail.com>
2018-03-20 13:07:20 +0530

    Added functionality to check account balance

--- library/src/main/java/com/thoughtworks/step/bank/Account.java
+++ library/src/main/java/com/thoughtworks/step/bank/Account.java
@@ -5,12 +5,15 @@ public class Account {
     private float balance;
 
     public Account(String accountNumber, float balance) {
-
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
     public String getAccountNumber() {
         return accountNumber;
+    }
+
+    public float getAccountBalance() {
+        return balance;
     }
 }
--- library/src/test/java/AccountTest.java
+++ library/src/test/java/AccountTest.java
@@ -1,13 +1,27 @@
 import com.thoughtworks.step.bank.Account;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AccountTest {
+
+    private Account account;
+
+    @Before
+    public void setUp() throws Exception {
+        account = new Account("1212-1212", 1000);
+    }
+
     @Test
     public void checkAccountNumber() {
-        Account account = new Account("1212-1212", 1000);
         assertThat(account.getAccountNumber(),is("1212-1212"));
     }
+
+    @Test
+    public void checkAccountBalance() {
+        assertThat(account.getAccountBalance(),is(1000.0f));
+    }
+
 }
