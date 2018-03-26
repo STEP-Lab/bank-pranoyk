fe043d7b4bbdf55d036be6e81319770d6836faac
Pranoy Kumar Kundu <pranoy1998k@gmail.com>
2018-03-26 12:08:23 +0530

    Added a test to check credit and debit transactions in one test

--- library/src/test/java/com/thoughtworks/step/bank/TransactionsTest.java
+++ library/src/test/java/com/thoughtworks/step/bank/TransactionsTest.java
@@ -1,4 +1,5 @@
 package com.thoughtworks.step.bank;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
@@ -8,17 +9,32 @@ import static org.junit.Assert.assertThat;
 
 public class TransactionsTest {
 
+    private Transactions transactions;
+
+    @Before
+    public void setUp() throws Exception {
+        transactions = new Transactions();
+    }
+
     @Test
     public void mustRecordDebitTransaction() {
-        Transactions transactions = new Transactions();
         transactions.debit(1000, "Pranav");
         assertThat(transactions.list,hasItem(new DebitTransaction(new Date(),1000,"Pranav")));
     }
 
     @Test
     public void mustRecordCreditTransaction() {
-        Transactions transactions = new Transactions();
         transactions.credit(1000, "Pranav");
         assertThat(transactions.list,hasItem(new CreditTransaction(new Date(),1000,"Pranav")));
+    }
+
+    @Test
+    public void mustRecordBothCreditAndDebitTransaction(){
+        transactions.debit(1000,"Pranav");
+        Date debitDate = transactions.list.get(0).getDate();
+        transactions.credit(1000,"Ashish");
+        Date creditDate = transactions.list.get(1).getDate();
+        assertThat(transactions.list, hasItem(new DebitTransaction(debitDate,1000,"Pranav")));
+        assertThat(transactions.list, hasItem(new CreditTransaction(creditDate,1000,"Ashish")));
     }
 }
